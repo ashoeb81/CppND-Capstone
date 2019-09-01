598d0175ebc29f58e0ea2b138a33fe644af48492
Ali Shoeb <ashoeb81@gmail.com>
2019-09-01 23:02:57 +0000

    Adding Boost lib and project video

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main() {
+#include <boost/program_options.hpp>
+using namespace boost::program_options;
+
+int main(int argc, char* argv[]) {
+    string fileName;
+    options_description desc("Allowed options");
+    desc.add_options()
+      ("file_name", value(&fileName), "pathname for output")
+      ;
+    
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+          
     std::cout << "Hello World!" << "\n";
+    std::cout << "File Name: " << vm["file_name"].as<std::string>() << std::endl;
     return 0;
 }
